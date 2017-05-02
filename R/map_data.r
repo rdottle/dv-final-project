48b7a51a7be9e566a8f00ca3dcc256b72b9941f4
rdottle <rachaelcdottle@gmail.com>
2017-05-02 16:40:56 -0400

    add word freq geojson again?

--- R/map_data.r
+++ R/map_data.r
@@ -135,3 +135,105 @@ nta_join2$barfreq <- nta_join2$barfreq/nta_join2$n * 100
 nta_join2$parkfreq <- nta_join2$parkfreq/nta_join2$n * 100
 
 
+
+
+
+########################### LISTINGS ################################################
+
+
+
+text_listings <- revslist_bynab %>% unnest_tokens(word, summary)
+
+data("stop_words")
+
+stop_words
+cleaned_list <- anti_join(text_listings, stop_words, by = c("word" = "word"))
+
+count_listing_words <- cleaned_list %>% group_by(ntacode) %>% summarise(n = n())
+
+#memory.limit(size = 30000)
+
+top_listing_words <- cleaned_list %>% group_by(ntaname) %>% count(word, sort = TRUE)
+
+hipsterlist_words <- top_listing_words[grep("hipster", top_listing_words$word), ]
+historiclist_words <- top_listing_words[grep("historic", top_listing_words$word), ]
+diverselist_words <- top_listing_words[grep("diverse", top_listing_words$word), ]
+
+cozylist_words <- top_listing_words[grep("cozy", top_listing_words$word), ]
+
+modernlist_words <- top_listing_words[grep("modern", top_listing_words$word), ]
+
+affordablelist_words <- top_listing_words[grep("affordable", top_listing_words$word), ]
+
+luxurylist_words <- top_listing_words[grep("luxury", top_listing_words$word), ]
+
+busylist_words <- top_listing_words[grep("busy", top_listing_words$word), ]
+quietlist_words <- top_listing_words[grep("quiet", top_listing_words$word), ]
+
+barlist_words <- top_listing_words[grep("bar", top_listing_words$word), ]
+parklist_words <- top_listing_words[grep("park", top_listing_words$word), ]
+
+
+hipsterlist <- hipsterlist_words %>% group_by(ntaname) %>% summarise(hipsterfreq = sum(n))
+
+historiclist <- historiclist_words %>% group_by(ntaname) %>% summarise(historicfreq = sum(n))
+
+diverselist <- diverselist_words %>% group_by(ntaname) %>% summarise(diversefreq = sum(n))
+
+cozylist <- cozylist_words %>% group_by(ntaname) %>% summarise(cozyfreq = sum(n))
+
+modernlist <- modernlist_words%>% group_by(ntaname) %>% summarise(modernfreq = sum(n))
+
+affordablelist <- affordablelist_words %>% group_by(ntaname) %>% summarise(affordablefreq = sum(n))
+
+luxurylist <- luxurylist_words %>% group_by(ntaname) %>% summarise(luxuryfreq = sum(n))
+
+busylist <- busylist_words %>% group_by(ntaname) %>% summarise(busyfreq = sum(n))
+
+quietlist <-  quietlist_words %>% group_by(ntaname) %>% summarise(quietfreq = sum(n))
+
+barlist <- barlist_words %>% group_by(ntaname) %>% summarise(barfreq = sum(n))
+
+parklist <- parklist_words %>% group_by(ntaname) %>% summarise(parkfreq = sum(n))
+
+
+nta_joinlist <- nta %>% left_join(hipsterlist, by = "ntaname") %>% left_join(historiclist, by = "ntaname") %>% left_join(diverselist, by = "ntaname") %>%
+  left_join(cozylist, by = "ntaname") %>% left_join(modernlist, by = "ntaname") %>% left_join(affordablelist, by = "ntaname") %>% left_join(luxurylist, by = "ntaname") %>%
+  left_join(busylist, by = "ntaname") %>% left_join(quietlist, by = "ntaname") %>% left_join(barlist, by = "ntaname") %>% left_join(parklist, by = "ntaname")
+
+
+
+nta_joinlist <- nta_joinlist %>% select(ntacode, county_fips, ntaname, hipsterfreq, historicfreq, diversefreq, 
+                                  cozyfreq, modernfreq, affordablefreq, luxuryfreq, busyfreq, quietfreq, barfreq, parkfreq)
+
+
+nta_joinlist[is.na(nta_joinlist)] <- 0
+nta_joinlist <- left_join(nta_joinlist, count_listing_words, by = "ntacode")
+
+
+nta_joinlist$hipsterlistfreq <- nta_joinlist$hipsterfreq/nta_joinlist$n
+nta_joinlist$hipsterlistfreq <- nta_joinlist$hipsterlistfreq*100
+
+nta_joinlist$historiclistfreq <- nta_joinlist$historicfreq/nta_joinlist$n * 100
+nta_joinlist$diverselistfreq <- nta_joinlist$diversefreq/nta_joinlist$n * 100
+
+nta_joinlist$cozylistfreq <- nta_joinlist$cozyfreq/nta_joinlist$n * 100
+nta_joinlist$modernlistfreq <- nta_joinlist$modernfreq/nta_joinlist$n * 100
+
+nta_joinlist$affordablelistfreq <- nta_joinlist$affordablefreq/nta_joinlist$n * 100
+nta_joinlist$luxurylistfreq <- nta_joinlist$luxuryfreq/nta_joinlist$n * 100
+
+nta_joinlist$busylistfreq <- nta_joinlist$busyfreq/nta_joinlist$n * 100
+nta_joinlist$quietlistfreq <- nta_joinlist$quietfreq/nta_joinlist$n * 100
+
+nta_joinlist$barlistfreq <- nta_joinlist$barfreq/nta_joinlist$n * 100
+nta_joinlist$parklistfreq <- nta_joinlist$parkfreq/nta_joinlist$n * 100
+
+nta_joinlist <- nta_joinlist %>% select(ntacode, county_fips, ntaname, hipsterlistfreq, historiclistfreq, diverselistfreq, 
+                                               cozylistfreq, modernlistfreq, affordablelistfreq, luxurylistfreq, busylistfreq, quietlistfreq, barlistfreq, parklistfreq, n)
+
+nta_joinlist[is.na(nta_joinlist)] <- 0
+
+
+write.csv(nta_joinlist, "nta_joinlist.csv")
+getwd()
